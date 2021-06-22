c155759313980617cbd9fb180fb23f1cc526feb4
Lunastellia <79289200+Lunastellia@users.noreply.github.com>
2021-06-22 04:27:09 +0200

    Add files via upload

---
+++ BetterSkeldPlugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+using HarmonyLib;
+using Reactor;
+using Reactor.Patches;
+
+
+
+namespace ChallengerBetterSkeld
+{
+    [BepInPlugin(Id)]
+    [BepInProcess("Among Us.exe")]
+    [BepInDependency(ReactorPlugin.Id)]
+    public class BetterSkeldPlugin : BasePlugin
+    {
+        public const string Id = "challenger.Lunastellia.betterSkeld";
+
+        public Harmony Harmony { get; } = new Harmony(Id);
+        public static ManualLogSource log;
+
+
+        public override void Load()
+        
+        {
+            log = Log;
+            log.LogMessage("Challenger Better Skeld Mod loaded");
+            
+            Harmony.PatchAll();
+        }
+    }
+}
+
